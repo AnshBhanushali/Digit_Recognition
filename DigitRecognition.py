f117bf3860c8ce46bbdb4c2511027335500252e9
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-06-03 01:00:27 -0400

    commit

--- DigitRecognition.py
+++ DigitRecognition.py
@@ -4,3 +4,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
+mnist = tf.keras.dataset.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalise(x_train, axis = 1)
+x_test = tf.keras.utils.normalise(x_test, axis = 1)
+
+model = tf.keras.models.Sequential()
+model.add(tf.Keras.layers.Flatten(input_shape=(28,28)))
+model.add(tf.Keras.layers.Dense(128, activation='relu'))
+model.add(tf.Keras.layers.Dense(128, activation='relu'))
+model.add(tf.Keras.layers.Dense(10, activation='softmax'))
+
+
+
+
+
